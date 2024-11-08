fcdcbf01c32854a2dec5579336d57cd7bcd974d2
Tawab Baah <tawabbaah35@gmail.com>
2024-11-08 09:06:25 +0000

    Initialize urls

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,10 @@
+from .views import list_books
+from .views import LibraryDetailView
+from django.urls import path
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/', LibraryDetailView.as_view(), name='library'),
+]
+
+
