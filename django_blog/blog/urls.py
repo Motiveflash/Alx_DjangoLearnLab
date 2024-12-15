d1e3e71feb5c2653da16b2fc248f9eae30b6372c
Tawab Baah <tawabbaah35@gmail.com>
2024-12-15 15:56:59 +0000

    Debuged urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     # Blog Post CRUD Views
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
